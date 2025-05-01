bbf62a97bcb60035d205cc61f7e8b01a24cffefd
yeongbin <baack7700@hanyang.ac.kr>
2025-05-01 17:19:32 +0900

    Revise the code for encryption and decryption with password and add new logic for validation of service key

--- data/crawling.py
+++ data/crawling.py
@@ -5,11 +5,11 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-def encrypt(filename, password):
-    with open(filename, "rb") as file:
-        file_data = file.read()
-        file.close()
-    num_bytes = bytes(password,"utf-8")
+
+# encrypt service key with password and save encrypted info as txt file.
+def encrypt(filename, file_data, password):
+    num_bytes = bytes(password, "utf-8")
+    file_data = bytes(file_data, "utf-8")
     salt = bytes(16)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -19,12 +19,14 @@ def encrypt(filename, password):
     )
     key = base64.urlsafe_b64encode(kdf.derive(num_bytes))
     f = Fernet(key)
+    print(type(file_data))
     encrypted_data = f.encrypt(file_data)
     with open(filename, "wb") as file:
         file.write(encrypted_data)
         file.close()
     print("Success encryption for service_key")
 
+# decrypt txt file with password and obtain service key
 def decrypt(filename, password):
     num_bytes = bytes(password,"utf-8")
     salt = bytes(16)
@@ -47,18 +49,25 @@ def decrypt(filename, password):
         print("Invalid token, most likely the password is incorrect")
         return
     print("File decrypted successfully")
-    with open(filename, "wb") as file:
-        file.write(decrypted_data)
-        file.close()
-    return decrypted_data
+    s_key_str = str(decrypted_data, 'utf-8')
+    return s_key_str
 
-password = input("Generate the password for the database: ")
-s_key_bytes = decrypt('service_key.txt', password)
-encrypt('service_key.txt', password)
 
-s_key_str = str(s_key_bytes, 'utf-8')
+res = input("Have you already entered the service key YES: 1, NO: 2 \n")
+# password = input("Input the password for obtaining service key: ")
+if res == "1":
+    password = input("Input the password for obtaining service key: ")
+    s_key = decrypt('service_key.txt', password)
+elif res == "2":
+    s_key = input("Input service key:")
+    password = input("Input the password for saving service key: ")
+    encrypt('service_key.txt', s_key, password)
+else:
+    print("Invalid input....")
+    exit()
+
 url = 'http://apis.data.go.kr/1360000/AsosHourlyInfoService/getWthrDataList'
-params ={'serviceKey' : s_key_str, 'pageNo' : '365', 'numOfRows' : '10', 'dataType' : 'JSON', 'dataCd' : 'ASOS', 'dateCd' : 'HR', 'startDt' : '20200101', 'startHh' : '01', 'endDt' : '20200601', 'endHh' : '02', 'stnIds' : '108' }
+params ={'serviceKey' : s_key, 'pageNo' : '365', 'numOfRows' : '10', 'dataType' : 'JSON', 'dataCd' : 'ASOS', 'dateCd' : 'HR', 'startDt' : '20200101', 'startHh' : '01', 'endDt' : '20200601', 'endHh' : '02', 'stnIds' : '108' }
 
 response = requests.get(url, params=params)
 print(response.content)
